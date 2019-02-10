bc1db3ce4c5e4b1ea35eb1de6c07557c3bce6b83
Brian Dillingham <bdillingham88@gmail.com>
2019-02-10 16:15:42 -0500

    wip

--- src/AjaxSelect.php
+++ src/AjaxSelect.php
@@ -6,6 +6,10 @@ use Laravel\Nova\Fields\Field;
 
 class AjaxSelect extends Field
 {
+    public $showOnIndex = false;
+
+    public $showOnDetail = false;
+
     public $component = 'nova-ajax-select';
 
     public function get($endpoint)
