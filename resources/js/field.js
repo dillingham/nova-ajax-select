684b35c825c97da4e0a38ea16179c89017d3dfcb
Crayon <darkspear123@outlook.com>
2021-03-21 17:31:16 +0200

    Add detail view

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,3 +1,4 @@
 Nova.booting((Vue, router, store) => {
     Vue.component('form-nova-ajax-select', require('./components/FormField'))
+    Vue.component('detail-additional-fields', require('./components/DetailField'))
 })
